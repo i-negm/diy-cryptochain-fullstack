5bd951e2f6720dbd1bf90513da2d39f5cab3df6e
Islam Negm <islam.g.negm@gmail.com>
2021-04-29 05:31:15 +0200

    Add express framework and implement initial APIs for mining a new block + displaying all the current blocks in JSON format

---
+++ src/express/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain');
+
+const app = express();
+const blockchain = new Blockchain();
+
+/**
+ * Express Middle Ware Initialization
+ */
+app.use(bodyParser.json());
+
+app.get('/api/blocks', (req, res) => {
+  res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  // Redirect the user after sending the data
+  res.redirect('/api/blocks');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening at localhost:${PORT}`));
