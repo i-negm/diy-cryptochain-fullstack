8163e7d24cd266822fef7c030b4bdd8abd26cf23
Islam Negm <islam.g.negm@gmail.com>
2021-05-01 00:18:30 +0200

    Allow each peer node to sync out the current chain from already known root node

--- src/express/index.js
+++ src/express/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const PubSub = require('../pubsub/pubsub');
@@ -7,7 +8,8 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 /**
  * Express Middle Ware Initialization
  */
@@ -25,10 +27,22 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks');
 });
 
-const DEFAULT_PORT = 3000;
+const synchChains = () => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const rootChain = JSON.parse(body);
+      console.log('[DEBUG] Replace chain on sync with', rootChain);
+      blockchain.replaceChain(rootChain);
+    }
+  });
+};
+
 let PEER_PORT;
 if (process.env.GENERATE_PEER_PORT === 'true') {
   PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => console.log(`Listening at localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Listening at localhost:${PORT}`);
+  synchChains(); 
+});
