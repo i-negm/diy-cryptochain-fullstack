0cf5f09adc3d13b0a166c7d466063c980890623a
Islam Negm <islam.g.negm@gmail.com>
2021-06-18 04:11:15 +0200

    Add POST `/api/transact` to create a new transaction and push it to the local pool

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -25,6 +29,20 @@ app.post('/api/mine', (req, res) => {
   pubsub.broadcastChain();  
   // Redirect the user after sending the data
   res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+  const {amount, recipient} = req.body;
+  let transaction;
+  try {
+    transaction = wallet.createTransaction({ recipient , amount });
+  } catch(err) {
+    console.error('/api/transact', err.message);
+    return res.status(400).json({ type: "error", message: err.message });
+  }
+  transactionPool.setTransaction(transaction);
+  console.log('transactionPool', transactionPool);
+  res.json({ type: "success", transaction });
 });
 
 const synchChains = () => {
