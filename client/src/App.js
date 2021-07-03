b85c2b75c4027b857b405630bb64f4844fa8169d
Islam Negm <islam.g.negm@gmail.com>
2021-07-03 16:35:32 +0200

    Add bootstrap and some styling for the blocks

--- client/src/App.js
+++ client/src/App.js
@@ -15,14 +15,16 @@ class App extends Component {
   render() {
     const {address, balance} = this.state.walletInfo;
     return (
-      <div>
-        <div>
-          Welcome to the crypto blockchain...
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <div> Address {address? address.substring(0,11): address} </div>
+            <div> Balance {balance} </div>
+          </div>
+          <div className="col-9">
+            <Blocks />
+          </div>
         </div>
-        <div> Address {address} </div>
-        <div> Balance {balance} </div>
-        <br />
-        <Blocks />
       </div>
     );
   }
