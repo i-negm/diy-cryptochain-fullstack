81ec2671c86fac045df26ffcc4b711bac098f698
Islam Negm <islam.g.negm@gmail.com>
2021-07-03 17:40:15 +0200

    Display the full data of the block and also the nonce and the difficulty level

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -3,14 +3,15 @@ import React, {Component} from 'react';
 
 class Block extends Component {
   render() {
-    const {timestamp, hash, data} = this.props.block;
+    const {timestamp, hash, data, nonce, difficulty} = this.props.block;
     const {blockIndex, chainLength} = this.props.metaData;
 
     const hashDisplay = `${hash.substring(0, 15)}...`;
     const blockHash =  hash ? hash.substring(0,15): hash; ;
 
-    const stringifiedData = JSON.stringify(data);
-    const dataDisplay = stringifiedData.length > 35? `${stringifiedData.substring(0, 35)}...`: stringifiedData;
+    const stringifiedData = JSON.stringify(data, null, 4);
+    // const dataDisplay = stringifiedData.length > 35? `${stringifiedData.substring(0, 35)}...`: stringifiedData;
+    const dataDisplay = stringifiedData;
 
     return(
       <div className="accordion-item">
@@ -21,9 +22,11 @@ class Block extends Component {
         </h2>
         <div id={"panelsStayOpen-collapse" + blockHash} className="accordion-collapse collapse" aria-labelledby={"panelsStayOpen-" + blockHash}>
           <div className="accordion-body">
-            <div>Hash: {hashDisplay}</div>
-            <div>timestamp: {new Date(timestamp).toLocaleString()}</div>
-            <div>Data: {dataDisplay}</div>
+            <div><strong>timestamp:</strong> {new Date(timestamp).toLocaleString()}</div>
+            <div><strong>Hash:</strong> {hash}</div>
+            <div><strong>Nonce:</strong> {nonce}</div>
+            <div><strong>Difficulty:</strong> {difficulty}</div>
+            <div><strong>Data:</strong> <pre>{dataDisplay}</pre></div>
           </div>
         </div>
       </div>
